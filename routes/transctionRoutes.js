d36bca49dbe71289ed517f3c71879d29af871616
unknown <shreyabhale11@gmail.com>
2023-09-23 23:12:14 +0530

    Second commit

--- routes/transctionRoutes.js
+++ routes/transctionRoutes.js
@@ -1,22 +1,25 @@
-const express = require('express');
-const { addTransctions, getAllTransctions, editTransctions, deleteTransctions } = require('../controllers/transactionController');
+const express = require("express");
+const {
+  addTransctions,
+  getAllTransctions,
+  editTransctions,
+  deleteTransctions,
+} = require("../controllers/transactionController");
 
 //router obj
 const router = express.Router();
 
 //routers
 //add trans || post
-router.post('/add-transaction', addTransctions);
+router.post("/add-transaction", addTransctions);
 
 //edit
-router.post('/edit-transaction', editTransctions);
+router.post("/edit-transaction", editTransctions);
 
 //delete
-router.post('/delete-transaction', deleteTransctions);
-
+router.post("/delete-transaction", deleteTransctions);
 
 //get trans || get
-router.post('/get-transaction', getAllTransctions);
-
+router.post("/get-transaction", getAllTransctions);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
-const colors = require('colors');
+const path = require('path');
 const connectDB = require('./config/connection');
 
 //congig dot env file
@@ -22,6 +22,13 @@ app.use(cors());
 //routes
 app.use('/api/v1/users', require('./routes/userRoute'));
 app.use('/api/v1/transactions', require('./routes/transctionRoutes'));
+
+//static files
+app.use(express.static(path.join(__dirname, './client/build')));
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
 
 
 //port
